94a58731f51cec60951d6aeaa27f63f7c56873c4
timonpalm <timon.palm@gmx.de>
2024-09-05 11:03:07 +0200

    bug in calc targettime

--- src/utils/calcTargetDate.js
+++ src/utils/calcTargetDate.js
@@ -1,24 +1,13 @@
 export function calcTargetDate (targetWeekday, targetTime) {
   var targetDate = new Date()
 
-  //console.log('now: ' + targetDate)
-  //console.log('targetweekday: ' + targetWeekday)
-  //console.log('targetTime: ' + targetTime)
-
-  //console.log('date: ' + targetDate.getDate())
-  //console.log('date+1: ' + (targetDate.getDate() + 1))
-  //console.log('now date: ' + targetDate.getDate())
-  //console.log((targetWeekday + 7 - targetDate.getDay()) % 7 || 7)
+  const diffdays = (parseInt(targetWeekday) + 7 - targetDate.getDay()) % 7
 
   // calculate target date from weekday
-  targetDate.setDate(
-    targetDate.getDate() + ((targetWeekday + 7 - targetDate.getDay()) % 7 || 7)
-  )
+  targetDate.setDate(targetDate.getDate() + (diffdays || 7))
   const [hours, minutes] = targetTime.split(':')
-  //console.log('hours: ' + hours)
-  targetDate.setHours(hours, minutes, 0, 0)
 
-  //console.log(targetDate)
+  targetDate.setHours(hours, minutes, 0, 0)
 
   return targetDate
 }
